eb94f63033ec74b6d89cb33020e24e38318523cc
Emmanuel <manuboisset@gmail.com>
2023-06-28 21:45:08 +0200

    update friend request model

--- src/app/services/friends/friends.service.ts
+++ src/app/services/friends/friends.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { FriendsModel } from '../../models/friends.model';
+import { FriendRequestModel } from '../../models/friend-request.model';
 
 const URL = environment.apiUrl + '/friends';
 
@@ -9,7 +9,7 @@ const URL = environment.apiUrl + '/friends';
     providedIn: 'root',
 })
 export class FriendsService {
-    friends: FriendsModel | null = null;
+    friends: FriendRequestModel | null = null;
 
     constructor(private http: HttpClient) {}
 
